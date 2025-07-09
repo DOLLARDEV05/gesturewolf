4068c5ee85f32ddb312c141631777457a0185c97
DOLLARDEV05 <dollardev05@gmail.com>
2025-07-09 19:32:46 +0530

    using the split() function for better catching and avoiding misinterpretations like "13" for 3

--- gesturewolf.py
+++ gesturewolf.py
@@ -5,25 +5,37 @@ proc = subprocess.Popen(["sudo", "libinput", "debug-events"],
                         stdout=subprocess.PIPE, text=True)
 
 active_fingers = None
+
 for line in proc.stdout:
+    
     if "GESTURE_SWIPE_BEGIN" in line:
-        if "3" in line:
+        parts = line.split()
+        
+        if "3" in parts:
             print("3 Finger swipe started\n")
             active_fingers = 3
-        elif "4" in line:
+        
+        elif "4" in parts:
             print("4 Finger swipe started\n")
             active_fingers = 4
+
     elif "GESTURE_SWIPE_UPDATE" in line:
+        
         if 3 == active_fingers:
             print("3 finger swiping\n")
+        
         elif 4 == active_fingers:
             print("4 figer swiping\n")
+    
     elif "GESTURE_SWIPE_END" in line:
+        
         if  3 == active_fingers:
             print("3 finger swipe ended\n")
             active_fingers =None
+        
         elif 4 == active_fingers:
             print("4 finger swipe ended\n")
             active_fingers =None
+    
     else:
         print(line)
