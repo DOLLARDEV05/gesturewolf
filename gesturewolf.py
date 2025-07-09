7970b0c3addaaaafdd43e4c0aee9b17090e674cc
DOLLARDEV05 <dollardev05@gmail.com>
2025-07-09 19:00:27 +0530

    basic version to log 3,4 finger swipe starting and endings

--- gesturewolf.py
+++ gesturewolf.py
@@ -5,11 +5,20 @@ proc = subprocess.Popen(["sudo", "libinput", "debug-events"],
                         stdout=subprocess.PIPE, text=True)
 
 for line in proc.stdout:
-    if "GESTURE_SWIPE_BEGIN" in line  and "3" in line:
-        print("3 Finger swipe started\n")
-    elif "GESTURE_SWIPE_UPDATE" in line and "3" in line:
-        print("3 finger swiping\n")
-    elif "GESTURE_SWIPE_END" in line and "3" in line:
-        print("3 finger swipe ended")
+    if "GESTURE_SWIPE_BEGIN" in line:
+        if "3" in line:
+            print("3 Finger swipe started\n")
+        elif "4" in line:
+            print("4 Finger swipe started\n")
+    elif "GESTURE_SWIPE_UPDATE" in line:
+        if "3" in line:
+            print("3 finger swiping\n")
+        elif "4" in line:
+            print("4 figer swiping\n")
+    elif "GESTURE_SWIPE_END" in line:
+        if  "3" in line:
+            print("3 finger swipe ended\n")
+        elif "4" in line:
+            print("4 finger swipe ended\n")
     else:
         print(line)
