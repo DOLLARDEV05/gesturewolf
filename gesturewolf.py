c98e453c9eae8a90c2c3cd484067df6478e3651d
DOLLARDEV05 <dollardev05@gmail.com>
2025-07-09 19:16:32 +0530

    fixed mid gesture noise 3-4 fingers count change mid gesture

--- gesturewolf.py
+++ gesturewolf.py
@@ -4,21 +4,26 @@ import subprocess
 proc = subprocess.Popen(["sudo", "libinput", "debug-events"],
                         stdout=subprocess.PIPE, text=True)
 
+active_fingers = None
 for line in proc.stdout:
     if "GESTURE_SWIPE_BEGIN" in line:
         if "3" in line:
             print("3 Finger swipe started\n")
+            active_fingers = 3
         elif "4" in line:
             print("4 Finger swipe started\n")
+            active_fingers = 4
     elif "GESTURE_SWIPE_UPDATE" in line:
-        if "3" in line:
+        if 3 == active_fingers:
             print("3 finger swiping\n")
-        elif "4" in line:
+        elif 4 == active_fingers:
             print("4 figer swiping\n")
     elif "GESTURE_SWIPE_END" in line:
-        if  "3" in line:
+        if  3 == active_fingers:
             print("3 finger swipe ended\n")
-        elif "4" in line:
+            active_fingers =None
+        elif 4 == active_fingers:
             print("4 finger swipe ended\n")
+            active_fingers =None
     else:
         print(line)
